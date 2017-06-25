c1b5b1b684f2d66e3aeb1b857fa97b6efe97c8fd
Rob Dobson <rob@dobson.com>
2017-06-25 10:38:44 +0100

    Added minify to HTML hex generation

--- resources/GenResources.py
+++ resources/GenResources.py
@@ -1,6 +1,14 @@
 
+import logging as log
+import os, os.path
+import shutil
+import subprocess
+MINIFY_HTML = True
 
-import os.path
+# NOTE that if MINIFY_HTML is True then the Node package html-minifier needs to be installed globally
+# npm install html-minifier -g
+
+log.basicConfig(level=log.DEBUG)
 
 # f = open("res/favicon.ico")
 # for s in f:
@@ -18,8 +26,49 @@ import os.path
 # for each file in "./res":
 #
 
-resFileInfo = []
+def writeFileContentsAsHex(filePath, outFile):
+    filename, file_extension = os.path.splitext(filePath)
+    # print("File", filePath, "name", filename, "ext", file_extension)
+    inFileName = filePath
+    if MINIFY_HTML and file_extension.upper()[:4] == ".HTM":
+        print("Minifying")
+        inFileName = filename + ".tmp"
+        # Copy file
+        print("Copying", filePath, "to", inFileName)
+        shutil.copyfile(filePath, inFileName)
+        # Try to minify using npm html-minifier
+        print("Running minifier on", filePath)
+        rslt = subprocess.run(["html-minifier", filePath, "--minify-js", "--minify-css"], shell=True, stdout=subprocess.PIPE)
+        if (rslt.returncode == 0):
+            # print(rslt)
+            with open(inFileName, "wb") as text_file:
+                text_file.write(rslt.stdout)
+            print("Input HTML was",os.stat(filePath).st_size,"bytes, minified file is",os.stat(inFileName).st_size, "bytes")
+        else:
+            print("MINIFY FAILED returncode", rslt.returncode)
 
+    # Iterate through file in binary
+    chCount = 0
+    lineChIdx = 0
+    with open(inFileName, "rb") as inFile:
+        byte = inFile.read(1)
+        while byte:
+            if chCount != 0:
+                outFile.write(",")
+            if lineChIdx == 0:
+                outFile.write("\n" + " " * lineHexIndentChars)
+            outFile.write("0x" + format(ord(byte), "02x"))
+            byte = inFile.read(1)
+            lineChIdx += 1
+            if lineChIdx >= lineHexBytesLen:
+                lineChIdx = 0
+            chCount += 1
+
+    if MINIFY_HTML and file_extension.upper()[:4] == ".HTM":
+        print("Removing", inFileName)
+        os.remove(inFileName)
+
+resFileInfo = []
 lineNormalIndentChars = 4
 lineHexBytesLen = 16
 lineHexIndentChars = 4
@@ -38,22 +87,8 @@ with open("../src/GenResources.h", "w") as outFile:
             print(cIdent, fileName)
             # Write variable def
             outFile.write("static const uint8_t " + cIdent + "[] {")
-            # Iterate through file in binary
-            chCount = 0
-            lineChIdx = 0
-            with open(filePath, "rb") as inFile:
-                byte = inFile.read(1)
-                while byte:
-                    if chCount != 0:
-                        outFile.write(",")
-                    if lineChIdx == 0:
-                        outFile.write("\n" + " " * lineHexIndentChars)
-                    outFile.write("0x" + format(ord(byte), "02x"))
-                    byte = inFile.read(1)
-                    lineChIdx += 1
-                    if lineChIdx >= lineHexBytesLen:
-                        lineChIdx = 0
-                    chCount += 1
+            # Write file contents as hex
+            writeFileContentsAsHex(filePath, outFile)
             outFile.write("\n" + " " * lineHexIndentChars + "};\n\n")
             # Form the file info to be added to resources
             fileInfoRec = {
