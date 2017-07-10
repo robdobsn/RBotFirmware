7a4d9a6b89d7c7acd30ded61de9bc355167fef7e
Rob Dobson <rob@dobson.com>
2017-07-11 00:21:10 +0100

    Log a long string as multiple lines

--- src/Utils.h
+++ src/Utils.h
@@ -28,4 +28,22 @@ public:
         return 0;
         return maxDuration - (ULONG_MAX - (lastTime-curTime));
     }
+    static void logLongStr(const char* headerMsg, const char* toLog, bool infoLevel = false)
+    {
+        if (infoLevel)
+            Log.info(headerMsg);
+        else
+            Log.trace(headerMsg);
+        const int linLen = 80;
+        for (int i = 0; i < strlen(toLog); i+=linLen)
+        {
+            char pBuf[linLen+1];
+            strncpy(pBuf, toLog+i, linLen);
+            pBuf[linLen] = 0;
+            if (infoLevel)
+                Log.info(pBuf);
+            else
+                Log.trace(pBuf);
+        }
+    }
 };
