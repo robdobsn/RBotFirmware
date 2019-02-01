ad87a200573dde3e7269306a986520a76ca48586
Rob Dobson <rob@dobson.com>
2019-02-01 15:39:18 +0000

    Removed old Particle based firmware

--- ParticleSw/build.bat
+++
@@ -1,7 +0,0 @@
-call particle compile photon --target 0.7.0-rc.7
-for %%x in (*.bin) do (
-        call particle flash --usb %%x
-        goto breakout
-    )
-:breakout
-for %%x in (*.bin) do del %%x
--- ParticleSw/src/TestMotionActuator.cpp
+++
@@ -1,7 +0,0 @@
-// RBotFirmware
-// Rob Dobson 2016-2018
-
-#include "application.h"
-#include "TestMotionActuator.h"
-
-uint32_t TestMotionActuator::_testCount = 0;
